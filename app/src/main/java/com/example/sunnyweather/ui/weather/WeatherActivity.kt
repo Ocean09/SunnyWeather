2dcf56e24a6c901adfd1046587da75eb2a248937
xiaozr <xiaozuoren@foxmail.com>
2022-06-09 11:23:37 +0800

    finish change city

--- app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,14 +1,18 @@
 package com.example.sunnyweather.ui.weather
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.sunnyweather.R
@@ -56,9 +60,31 @@ class WeatherActivity : AppCompatActivity() {
         binding.swipeRefresh.setOnRefreshListener {
             refreshWeather()
         }
+
+        binding.weatherNowLayout.navBtn.setOnClickListener {
+            binding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        binding.drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerOpened(drawerView: View) {}
+
+            @SuppressLint("ServiceCast")
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
     }
 
-    private fun refreshWeather() {
+    fun closeDrawer() {
+        binding.drawerLayout.closeDrawers()
+    }
+
+    fun refreshWeather() {
         binding.swipeRefresh.isRefreshing = true
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
     }
